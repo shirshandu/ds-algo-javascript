c328eb9fe22426c9ab93f8817360a6dddb7d310a
shirshandu <shirshandu@yahoo.com>
2020-12-14 21:33:41 +0530

    palindrome

---
+++ palindrome/index.js
@@ -0,0 +1,14 @@
+// Solution 1
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+//   return str === reversed;
+// }
+
+// Solution 2
+function palindrome(str) {
+  return str.split('').every((char, i)  => {
+    return char === str[str.length -i -1]
+  })
+}
+
+module.exports = palindrome;
