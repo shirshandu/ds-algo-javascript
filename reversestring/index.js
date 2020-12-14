c02da7dbdf050f3ac0dc0e6674fa427b22b41095
shirshandu <shirshandu@yahoo.com>
2020-12-14 21:12:04 +0530

    reverse a string

--- reversestring/index.js
+++ reversestring/index.js
@@ -4,19 +4,19 @@
 // }
 
 // Solution 2
-// function reverse(str) {
-//   let reversed = "";
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
-//   return reversed;
-// }
+function reverse(str) {
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
 
 // Solution 3
-function reverse(str) {
-  return str.split("").reduce((reversed, character) => {
-    return character + reversed;
-  }, "");
-}
+// function reverse(str) {
+//   return str.split("").reduce((reversed, character) => {
+//     return character + reversed;
+//   }, "");
+// }
 
 module.exports = reverse;
