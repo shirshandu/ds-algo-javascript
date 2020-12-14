8083b2279aa138cec8ca4e674a578053bf426c6a
shirshandu <shirshandu@yahoo.com>
2020-12-14 21:09:44 +0530

    setup done, installled jest

---
+++ reversestring/index.js
@@ -0,0 +1,22 @@
+// Solution 1
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// Solution 2
+// function reverse(str) {
+//   let reversed = "";
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// Solution 3
+function reverse(str) {
+  return str.split("").reduce((reversed, character) => {
+    return character + reversed;
+  }, "");
+}
+
+module.exports = reverse;
